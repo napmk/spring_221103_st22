0ac752c2729ed588b84138589c322f7fce38e837
ici <ici@DESKTOP-J9CT40H>
2022-11-03 12:46:39 +0900

    트랜젝션 적용하기

--- src/main/java/com/napmkmk/st22/TicketDao.java
+++ src/main/java/com/napmkmk/st22/TicketDao.java
@@ -6,53 +6,71 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class TicketDao {
 	
 	JdbcTemplate template;
-
+	TransactionTemplate transactionTemplate;
+	
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 	}	
 	
-	
+
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
+
 	public TicketDao() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-
-
 	public void buyTicket(final TicketDto dto) {
 		
-		this.template.update(new PreparedStatementCreator() {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			
 			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
 				// TODO Auto-generated method stub
-				String sql = "insert into card (consumerid,amount) values (?,?)";
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, dto.getConsumerid());
-				pstmt.setString(2, dto.getAmount());
 				
-				return pstmt;
+				
+				template.update(new PreparedStatementCreator() {
+					
+					@Override
+					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+						// TODO Auto-generated method stub
+						String sql = "insert into card (consumerid,amount) values (?,?)";
+						PreparedStatement pstmt = con.prepareStatement(sql);
+						pstmt.setString(1, dto.getConsumerid());
+						pstmt.setString(2, dto.getAmount());
+						
+						return pstmt;
+					}
+				});
+				
+				template.update(new PreparedStatementCreator() {
+					
+					@Override
+					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+						// TODO Auto-generated method stub
+						String sql = "insert into ticket (consumerid,countnum) values (?,?)";
+						PreparedStatement pstmt = con.prepareStatement(sql);
+						pstmt.setString(1, dto.getConsumerid());
+						pstmt.setString(2, dto.getAmount());
+						
+						return pstmt;
+					}
+				});
+				
+				
 			}
 		});
 		
-		this.template.update(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				// TODO Auto-generated method stub
-				String sql = "insert into ticket (consumerid,countnum) values (?,?)";
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, dto.getConsumerid());
-				pstmt.setString(2, dto.getAmount());
-				
-				return pstmt;
-			}
-		});
-		
+	
 	}
 	
 }
